02747c66867c8c6e4bf467e8c18000ce1e31b317
Jordan Durci <durcij@aldenv104.allegheny.edu>
2018-12-10 22:07:15 -0500

    Made the Grumpa move correctly.

--- src/Operation Dessert Lasagna/Assets/Scripts/GrumpaMovement.cs
+++ src/Operation Dessert Lasagna/Assets/Scripts/GrumpaMovement.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class GrumpaMovement : MonoBehaviour {
 
 	private Vector3 MovingDirection = Vector3.left;    //initial movement direction
+  Animator anim;
+	bool isWalkingRight;
+	bool isWalkingLeft;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+		anim = GetComponent <Animator> ();
 	}
 
 	// Update is called once per frame
@@ -18,13 +22,22 @@ public class GrumpaMovement : MonoBehaviour {
 	void UpdateMovement(){
 			if (this.transform.position.x > 2.4f) {
 					MovingDirection = Vector3.left;
-					gameObject.GetComponent<SpriteRenderer> ().flipX = true;
+					isWalkingLeft = true;
+					isWalkingRight = false;
+					Animating (isWalkingLeft, isWalkingRight);
 
 			} else if (this.transform.position.x < -2f) {
 					MovingDirection = Vector3.right;
-					gameObject.GetComponent<SpriteRenderer> ().flipX = false;
+					isWalkingLeft = false;
+					isWalkingRight = true;
+					Animating (isWalkingLeft, isWalkingRight);
 
 			}
 			this.transform.Translate (MovingDirection * Time.smoothDeltaTime);
+	}
+
+	void Animating (bool isWalkingLeft, bool isWalkingRight)	{
+		anim.SetBool ("isWalkingLeft", isWalkingLeft);
+		anim.SetBool ("isWalkingRight", isWalkingRight);
 	}
 }

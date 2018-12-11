978477b806c53826e0d86dff497fd464f5cc869e
Noah Stape <stapen@aldenv105.allegheny.edu>
2018-12-10 21:15:29 -0500

    klj;sdfaljk;dfsjkl;sdfjkl;sdf(dying)

---
+++ src/Operation Dessert Lasagna/Assets/Scripts/GrumpaMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrumpaMovement : MonoBehaviour {
+
+	public float speed;
+	bool isMovedLeft = true;
+	bool isMovedRight = false;
+
+	void Awake () {
+	}
+	void Update () {
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (isMovedLeft = true) {
+			isMovedLeft = false;
+			isMovedRight = true; 
+		}
+	}
+}

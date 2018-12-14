843263d44de356957306f88d21b79dac2f87026d
Jordan Durci <durcij@aldenv105.allegheny.edu>
2018-12-14 11:26:13 -0500

    Armin do the punch now.

--- src/Operation Dessert Lasagna/Assets/Scripts/PlayerMovementArmin.cs
+++ src/Operation Dessert Lasagna/Assets/Scripts/PlayerMovementArmin.cs
@@ -11,6 +11,13 @@ public class PlayerMovementArmin : MonoBehaviour {
 	bool isInAir = false;
 	Animator anim;
 	AudioSource jumpAudio;
+	bool isPunchingRight;
+	bool isPunchingLeft;
+	bool hasPunched;
+	private int wait;
+	public float punchWait = 1.0f;
+	float timer;
+
 
 	void Awake () {
 		anim = GetComponent <Animator> ();
@@ -20,7 +27,9 @@ public class PlayerMovementArmin : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		speed = 3;
+		speed = 3.0f;
+
+		checkPunch ();
 
 		if (Input.GetKey(KeyCode.LeftArrow) && isInAir == true) // Move left
 			{
@@ -50,10 +59,20 @@ public class PlayerMovementArmin : MonoBehaviour {
 			GetComponent<Rigidbody2D>().AddForce(new Vector2(0,10), ForceMode2D.Impulse);
 			jumpAudio.Play ();
 		}
+
+		if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow) && hasPunched == false) {
+			isPunchingRight = true;
+			hasPunched = true;
+			AnimatingPunch (isPunchingRight, isPunchingLeft);
+		}	else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow) && hasPunched == false) {
+			isPunchingLeft = true;
+			hasPunched = true;
+			AnimatingPunch (isPunchingRight, isPunchingLeft);
+		}
 	}
 
 	// called when the character hits a thing
-  void OnCollisionEnter2D(Collision2D col)
+  void OnCollisionEnter2D (Collision2D col)
   {
 		isInAir = false;
   }
@@ -61,5 +80,21 @@ public class PlayerMovementArmin : MonoBehaviour {
 	void Animating (bool isMovedLeft, bool isMovedRight)	{
 		anim.SetBool ("isMovedLeft", isMovedLeft);
 		anim.SetBool ("isMovedRight", isMovedRight);
+	}
+
+	void AnimatingPunch (bool isPunchingRight, bool isPunchingLeft) {
+		anim.SetBool ("isPunchingRight", isPunchingRight);
+		anim.SetBool ("isPunchingLeft", isPunchingLeft);
+	}
+
+	void checkPunch () {
+		timer += Time.deltaTime;
+		if (timer >= punchWait) {
+			isPunchingLeft = false;
+			isPunchingRight = false;
+			AnimatingPunch (isPunchingRight, isPunchingLeft);
+			hasPunched = false;
+			timer = 0f;
+		}
 	}
 }
